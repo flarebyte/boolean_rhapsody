204f8d10edfeff2cb11a1ef42d5f31627216b3ff
Olivier Huin <olih@users.noreply.github.com>
2024-12-14 17:19:56 +0000

    IsAbsent function

--- lib/src/rule_model.dart
+++ lib/src/rule_model.dart
@@ -1,43 +1,49 @@
-class RhapsodyEvaluationContext { 
+class RhapsodyEvaluationContext {
   Map<String, String> variables;
   Map<String, String> constants;
   RhapsodyEvaluationContext({required this.variables, required this.constants});
 
-  String? getRefValue(String ref){
-    if (ref.startsWith('v:')){
+  String? getRefValue(String ref) {
+    if (ref.startsWith('v:')) {
       return variables[ref];
     }
-    if (ref.startsWith('c:')){
+    if (ref.startsWith('c:')) {
       return constants[ref];
     }
     throw Exception("The ref $ref should starts with v: or c:");
   }
-
 }
 
 abstract class BooleanRhapsodyFunction {
   bool isTrue(RhapsodyEvaluationContext context);
-  basicValidateParams(List<String> refs, int minSize, int maxSize){
+  basicValidateParams(
+      {required List<String> refs,
+      required int minSize,
+      required int maxSize,
+      required String name}) {
     final size = refs.length;
-    if (size<minSize){
-      throw Exception("Size was $size but was expecting a minimum of $minSize for $refs");
+    if (size < minSize) {
+      throw Exception(
+          "The number of parameters for the function $name was $size but was expecting a minimum of $minSize for $refs");
     }
-    if (size>maxSize){
-      throw Exception("Size was $size but was expecting a maximum of $maxSize for $refs");
+    if (size > maxSize) {
+      throw Exception(
+          "The number of parameters for the function $name was $size but was expecting a maximum of $maxSize for $refs");
     }
-    final hasUnsupportedPrefix = refs.where((param) => !(param.startsWith('v:')||(param.startsWith('c:')))).isNotEmpty;
-    if (hasUnsupportedPrefix){
-      throw Exception("The references should all starts with v: or c: for $refs");
+    final hasUnsupportedPrefix = refs
+        .where((param) => !(param.startsWith('v:') || (param.startsWith('c:'))))
+        .isNotEmpty;
+    if (hasUnsupportedPrefix) {
+      throw Exception(
+          "The references for the function $name should all starts with v: or c: for $refs");
     }
   }
 }
 
-
-
-class isUndefinedRhapsodyFunction extends BooleanRhapsodyFunction{
-  List<String> refs;
-  isUndefinedRhapsodyFunction({required this.refs}){
-    basicValidateParams(refs, 1, 1);
+class IsAbsentRhapsodyFunction extends BooleanRhapsodyFunction {
+  final List<String> refs;
+  IsAbsentRhapsodyFunction({required this.refs}) {
+    basicValidateParams(refs: refs, minSize: 1, maxSize: 1, name: 'isAbsent');
   }
 
   @override
@@ -45,13 +51,15 @@ class isUndefinedRhapsodyFunction extends BooleanRhapsodyFunction{
     final value = context.getRefValue(refs[0]);
     return value == null;
   }
-
 }
 
 class BooleanRhapsodyFunctionFactory {
-  
-  static BooleanRhapsodyFunction create(String name){
-
+  static BooleanRhapsodyFunction create(String name, List<String> params) {
+    switch (name) {
+      case 'isAbsent':
+        return IsAbsentRhapsodyFunction(refs: params);
+      default:
+        throw Exception("The boolean function $name is unknown");
+    }
   }
-
 }
