003de283833867a0af2776e8102209c8be94b377
Olivier Huin <olih@users.noreply.github.com>
2024-12-18 09:29:46 +0000

    Adds vars after device

--- lib/src/evaluation_context.dart
+++ lib/src/evaluation_context.dart
@@ -2,12 +2,12 @@ class RhapsodyEvaluationContext {
   Map<String, String> variables;
   Map<String, String> constants;
   Map<String, String> parameters;
-  Map<String, String> device;
+  Map<String, String> deviceVars;
   RhapsodyEvaluationContext(
       {required this.variables,
       required this.constants,
       required this.parameters,
-      required this.device});
+      required this.deviceVars});
 
   String? getRefValue(String ref) {
     if (ref.startsWith('v:')) {
@@ -20,7 +20,7 @@ class RhapsodyEvaluationContext {
       return parameters[ref];
     }
     if (ref.startsWith('d:')) {
-      return device[ref];
+      return deviceVars[ref];
     }
     throw Exception("The ref $ref should starts with v: c: p: or d:");
   }
