d438d7b4c657fb063c20c59f28c189cab784f854
Olivier Huin <olih@users.noreply.github.com>
2024-12-18 21:44:05 +0000

    Adds is present

--- lib/src/string_function.dart
+++ lib/src/string_function.dart
@@ -23,7 +23,7 @@ class IsAbsentRhapsodyFunction extends BooleanRhapsodyFunction {
   /// - [Exception] if [refs] does not contain exactly one reference.
   /// - [Exception] if the reference does not begin with `'v:'` or `'c:'`.
   IsAbsentRhapsodyFunction({required this.refs}) {
-    basicValidateParams(refs: refs, minSize: 1, maxSize: 1, name: 'isAbsent');
+    basicValidateParams(refs: refs, minSize: 1, maxSize: 1, name: 'is_absent');
   }
 
   /// Evaluates whether the reference specified in [refs] is absent (null)
@@ -38,3 +38,42 @@ class IsAbsentRhapsodyFunction extends BooleanRhapsodyFunction {
     return value == null;
   }
 }
+
+/// A boolean function that checks if a specified reference is present (not null)
+/// in the given [RhapsodyEvaluationContext].
+///
+/// This class extends [BooleanRhapsodyFunction] and performs a single
+/// validation during instantiation to ensure the provided parameters meet
+/// the function's requirements.
+class IsPresentRhapsodyFunction extends BooleanRhapsodyFunction {
+  /// A list of parameter references that the function evaluates.
+  ///
+  /// This function expects exactly one reference in the list.
+  final List<String> refs;
+
+  /// Creates an instance of [IsPresentRhapsodyFunction].
+  ///
+  /// [refs] - A list of references to be evaluated. The list must contain
+  /// exactly one reference, and the reference must begin with either `'v:'`
+  /// (variable) or `'c:'` (constant).
+  ///
+  /// Throws:
+  /// - [Exception] if [refs] does not contain exactly one reference.
+  /// - [Exception] if the reference does not begin with `'v:'` or `'c:'`.
+  IsPresentRhapsodyFunction({required this.refs}) {
+    basicValidateParams(refs: refs, minSize: 1, maxSize: 1, name: 'is_present');
+  }
+
+  /// Evaluates whether the reference specified in [refs] is present (not null)
+  /// in the provided [RhapsodyEvaluationContext].
+  ///
+  /// [context] - The evaluation context that provides data for the function.
+  ///
+  /// Returns `true` if the value of the reference is not `null`, otherwise `false`.
+  @override
+  bool isTrue(RhapsodyEvaluationContext context) {
+    final value = context.getRefValue(refs[0]);
+    return value != null;
+  }
+}
+
