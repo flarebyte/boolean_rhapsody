326aad56fc45ff049364c94928d75ba7fbf21ecf
Olivier Huin <olih@users.noreply.github.com>
2024-12-18 09:23:59 +0000

    Adds parameters and devices

--- lib/src/evaluation_context.dart
+++ lib/src/evaluation_context.dart
@@ -1,7 +1,13 @@
 class RhapsodyEvaluationContext {
   Map<String, String> variables;
   Map<String, String> constants;
-  RhapsodyEvaluationContext({required this.variables, required this.constants});
+  Map<String, String> parameters;
+  Map<String, String> device;
+  RhapsodyEvaluationContext(
+      {required this.variables,
+      required this.constants,
+      required this.parameters,
+      required this.device});
 
   String? getRefValue(String ref) {
     if (ref.startsWith('v:')) {
@@ -10,6 +16,12 @@ class RhapsodyEvaluationContext {
     if (ref.startsWith('c:')) {
       return constants[ref];
     }
-    throw Exception("The ref $ref should starts with v: or c:");
+    if (ref.startsWith('p:')) {
+      return parameters[ref];
+    }
+    if (ref.startsWith('d:')) {
+      return device[ref];
+    }
+    throw Exception("The ref $ref should starts with v: c: p: or d:");
   }
 }
