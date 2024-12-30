341d4bcd444f4e84dc809d478cefb8757f27ef15
Olivier Huin <olih@users.noreply.github.com>
2024-12-30 06:54:28 +0100

    Adds set comparator

---
+++ lib/src/set_comparator.dart
@@ -0,0 +1,53 @@
+abstract class RhapsodySetComparator {
+  /// The name of the comparator.
+  String get name;
+
+  /// Compares the given [value] against a [threshold].
+  ///
+  /// Returns `true` if the comparison meets the criteria, otherwise `false`.
+  bool compare(Set<String> value, Set<String> threshold);
+}
+
+// Comparator for checking if a value is equal to a threshold.
+class RhapsodySetEqualsToComparator extends RhapsodySetComparator {
+  @override
+  String get name => 'equals';
+
+  @override
+  bool compare(Set<String> value, Set<String> threshold) {
+    return value == threshold;
+  }
+}
+
+// Comparator for checking if a value is equal to a threshold.
+class RhapsodyIsSubsetOfComparator extends RhapsodySetComparator {
+  @override
+  String get name => 'is_subset_of';
+
+  @override
+  bool compare(Set<String> value, Set<String> threshold) {
+    return value.every(threshold.contains);
+  }
+}
+
+// Comparator for checking if a value is equal to a threshold.
+class RhapsodyIsSupersetOfComparator extends RhapsodySetComparator {
+  @override
+  String get name => 'is_superset_of';
+
+  @override
+  bool compare(Set<String> value, Set<String> threshold) {
+    return threshold.every(value.contains);
+  }
+}
+
+// Comparator for checking if a value is equal to a threshold.
+class RhapsodySetIsDisjointToComparator extends RhapsodySetComparator {
+  @override
+  String get name => 'is_disjoint';
+
+  @override
+  bool compare(Set<String> value, Set<String> threshold) {
+    return value.intersection(threshold).isEmpty;
+  }
+}
