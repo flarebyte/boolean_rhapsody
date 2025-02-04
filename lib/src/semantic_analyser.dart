9be0f41cc15bdc38e2e495d277970298dcee3814
Olivier Huin <olih@users.noreply.github.com>
2025-02-04 17:02:12 +0000

    Adds analysis failure

--- lib/src/semantic_analyser.dart
+++ lib/src/semantic_analyser.dart
@@ -1,17 +1,7 @@
 import 'rule_expession.dart';
 import 'token.dart';
 
-// class RhapsodyAnalysisFailure {
 
-//   RhapsodyPosition position;
-//   int index;
-//   String errorType;
-//   String message;
-//   String contextCode;
-//   String expected;
-//   String suggestion;
-
-// }
 
 // class RhapsodySemanticAnalysisResult {
 //   RhapsodyAnalysisFailure? failure;
