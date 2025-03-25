07debee92531b103b7136b0faaa5fcfb599d1cf2
Olivier Huin <olih@users.noreply.github.com>
2025-03-25 20:35:47 +0000

    Adds more test case for semantic analyser

--- test/semantic_analyser_test.dart
+++ test/semantic_analyser_test.dart
@@ -107,5 +107,89 @@ void main() {
       expect(analyzed.failure, isNotNull);
       expect(analyzed.failure?.message, equals('Expected \'rule\' keyword'));
     });
+
+    test('should return a failure if missing rule name after keyword', () {
+      final t = MockTokenCreator();
+      final tokens = [
+        t.token("rule", TokenTypes.identifier),
+        t.token("=", TokenTypes.equal),
+        t.token("rule42", TokenTypes.identifier),
+        t.token(";", TokenTypes.semicolon),
+      ];
+      final analyser = RhapsodySemanticAnalyser(fixtureMockOptions);
+      final result = analyser.analyse(tokens);
+      expect(result.failure, isNotNull);
+      expect(result.failure?.message,
+          equals("Expected an identifier for the rule name"));
+    });
+
+    test('should return a failure if rule name is not an identifier', () {
+      final t = MockTokenCreator();
+      final tokens = [
+        t.token("rule", TokenTypes.identifier),
+        t.token("=", TokenTypes.equal),
+        t.token("rule42", TokenTypes.identifier),
+        t.token(";", TokenTypes.semicolon),
+      ];
+      // Replace rule name token with an invalid type
+      tokens.insert(1, t.token("42", TokenTypes.number));
+      final analyser = RhapsodySemanticAnalyser(fixtureMockOptions);
+      final result = analyser.analyse(tokens);
+      expect(result.failure, isNotNull);
+      expect(result.failure?.message,
+          equals("Expected an identifier for the rule name"));
+    });
+    test('should return a failure if missing "=" after rule name', () {
+      final t = MockTokenCreator();
+      final tokens = [
+        t.token("rule", TokenTypes.identifier),
+        t.token("rule11", TokenTypes.identifier),
+        t.token("func1", TokenTypes.identifier),
+        t.token(";", TokenTypes.semicolon),
+      ];
+      final analyser = RhapsodySemanticAnalyser(fixtureMockOptions);
+      final result = analyser.analyse(tokens);
+      expect(result.failure, isNotNull);
+      expect(
+          result.failure?.message, equals("Expected '=' after the rule name"));
+    });
+    test('should return a failure if token after rule name is not "="', () {
+      final t = MockTokenCreator();
+      final tokens = [
+        t.token("rule", TokenTypes.identifier),
+        t.token("rule11", TokenTypes.identifier),
+        t.token(":", TokenTypes.colon), // Invalid here
+        t.token("func1", TokenTypes.identifier),
+        t.token(";", TokenTypes.semicolon),
+      ];
+      final analyser = RhapsodySemanticAnalyser(fixtureMockOptions);
+      final result = analyser.analyse(tokens);
+      expect(result.failure, isNotNull);
+      expect(
+          result.failure?.message, equals("Expected '=' after the rule name"));
+    });
+
+    test('should return a failure if missing semicolon at end of rule', () {
+      final t = MockTokenCreator();
+      final tokens = [
+        t.token("rule", TokenTypes.identifier),
+        t.token("rule11", TokenTypes.identifier),
+        t.token("=", TokenTypes.equal),
+        t.token("func1", TokenTypes.identifier),
+        t.token("(", TokenTypes.lparen),
+        t.token("env", TokenTypes.identifier),
+        t.token(":", TokenTypes.colon),
+        t.token("variable1", TokenTypes.identifier),
+        t.token(")", TokenTypes.rparen),
+        t.token("and", TokenTypes.operatorType),
+        t.token("rule42", TokenTypes.identifier),
+        // Missing semicolon
+      ];
+      final analyser = RhapsodySemanticAnalyser(fixtureMockOptions);
+      final result = analyser.analyse(tokens);
+      expect(result.failure, isNotNull);
+      expect(result.failure?.message,
+          equals("Expected ';' at the end of the rule definition"));
+    });
   });
 }
