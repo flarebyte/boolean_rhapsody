a6793f9be5d6097bd8e6b3236be9992974abc929
Olivier Huin <olih@users.noreply.github.com>
2025-03-25 16:49:36 +0000

    Test with two rules

--- test/semantic_analyser_test.dart
+++ test/semantic_analyser_test.dart
@@ -5,7 +5,7 @@ import 'code_fixtures.dart';
 
 void main() {
   group('RhapsodySemanticAnalyser', () {
-    test('should evaluate func1(env:variable1) and rule42;', () {
+    test('should evaluate a single rule', () {
       // func1(env:variable1) and rule42;
       final t = MockTokenCreator();
       final List<RhapsodyToken> tokens = [
@@ -28,6 +28,44 @@ void main() {
       expect(analyzed.failure, null);
       expect(analyzed.ruleDefinitions, hasLength(1));
       expect(analyzed.ruleDefinitions.keys, contains('rule11'));
+    });
+    test('should evaluate two rules', () {
+      // func1(env:variable1) and rule42;
+      final t = MockTokenCreator();
+      final List<RhapsodyToken> tokens = [
+        t.token("rule", TokenTypes.identifier),
+        t.token("rule11", TokenTypes.identifier),
+        t.token("=", TokenTypes.equal),
+        t.token("func1", TokenTypes.identifier),
+        t.token("(", TokenTypes.lparen),
+        t.token("env", TokenTypes.identifier),
+        t.token(":", TokenTypes.colon),
+        t.token("variable1", TokenTypes.identifier),
+        t.token(")", TokenTypes.rparen),
+        t.token("and", TokenTypes.operatorType),
+        t.token("rule42", TokenTypes.identifier),
+        t.token(";", TokenTypes.semicolon),
+        // second rule
+        t.token("rule", TokenTypes.identifier),
+        t.token("rule12", TokenTypes.identifier),
+        t.token("=", TokenTypes.equal),
+        t.token("func2", TokenTypes.identifier),
+        t.token("(", TokenTypes.lparen),
+        t.token("env", TokenTypes.identifier),
+        t.token(":", TokenTypes.colon),
+        t.token("variable4", TokenTypes.identifier),
+        t.token(")", TokenTypes.rparen),
+        t.token("and", TokenTypes.operatorType),
+        t.token("rule43", TokenTypes.identifier),
+        t.token(";", TokenTypes.semicolon),
+      ];
+      final RhapsodySemanticAnalyser analyser =
+          RhapsodySemanticAnalyser(fixtureMockOptions);
+      final analyzed = analyser.analyse(tokens);
+      expect(analyzed.failure, null);
+      expect(analyzed.ruleDefinitions, hasLength(2));
+      expect(analyzed.ruleDefinitions.keys, contains('rule11'));
+      expect(analyzed.ruleDefinitions.keys, contains('rule12'));
     });
   });
 }
