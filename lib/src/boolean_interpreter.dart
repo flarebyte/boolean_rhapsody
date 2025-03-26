9f52ae9e4871910bfeb7f0c4b1273f9f5a7233f0
Olivier Huin <olih@users.noreply.github.com>
2025-03-26 21:52:08 +0000

    Adds boolean_interpreter

---
+++ lib/src/boolean_interpreter.dart
@@ -0,0 +1,33 @@
+import 'evaluation_context.dart';
+import 'semantic_analyser.dart';
+
+class RhapsodyInterpreter {
+  RhapsodySemanticAnalysis analysis;
+  RhapsodyInterpreter(this.analysis) {
+    final invalid = analysis.failure != null ||
+        analysis.ruleDefinitions.isEmpty ||
+        analysis.orchestrator == null ||
+        analysis.orchestrator?.hasCycle == true;
+    if (invalid) {
+      throw Exception('The boolean rhapsody script is not valid');
+    }
+  }
+
+  interpret(RhapsodyEvaluationContext context) {
+    final parallelEval = analysis.orchestrator?.parallelEval ?? [];
+    final sequentialEval = analysis.orchestrator?.sequentialEval ?? [];
+
+    // Could be done in parallel but will probably add overhead.
+    for (var ruleName in parallelEval) {
+      final expression = analysis.ruleDefinitions[ruleName]!.expression;
+      final evaluated = expression.evaluate(context);
+      context.ruleState.set(ruleName, evaluated);
+    }
+    // This have to done sequentially
+    for (var ruleName in sequentialEval) {
+      final expression = analysis.ruleDefinitions[ruleName]!.expression;
+      final evaluated = expression.evaluate(context);
+      context.ruleState.set(ruleName, evaluated);
+    }
+  }
+}
--- todo.md
+++ todo.md
@@ -2,8 +2,7 @@
 - [ ] 100% cov
 - [ ] Clear public api: check, run (check first)
 - [ ] Expose less (files) ?
-- [ ] lib/src/graph_analyser.dart ? input is RhapsodySemanticAnalysisResult, output ?
-- [ ] Documentation
+- [ ] lib/src/boolean_interpreter.dart
 - [ ] Architecture diagram / Maintenance documentation
 - [ ] Example
 - [ ] Prepare extraction of Evaluation context ? variables & ruleState: diff or merge ? be compatible
