40160c7788a4a8a276930c68515a8a80c6dc7265
Olivier Huin <olih@users.noreply.github.com>
2025-07-11 06:11:46 +0100

    Uses composite variable in tests

--- test/boolean_interpreter_test.dart
+++ test/boolean_interpreter_test.dart
@@ -16,6 +16,8 @@ void main() {
         t.token("env", TokenTypes.identifier),
         t.token(":", TokenTypes.colon),
         t.token("variable1", TokenTypes.identifier),
+        t.token(":", TokenTypes.colon),
+        t.token("blue", TokenTypes.identifier),
         t.token(")", TokenTypes.rparen),
         t.token("and", TokenTypes.operatorType),
         t.token("rule2", TokenTypes.identifier),
@@ -28,7 +30,7 @@ void main() {
         t.token("(", TokenTypes.lparen),
         t.token("env", TokenTypes.identifier),
         t.token(":", TokenTypes.colon),
-        t.token("variable4", TokenTypes.identifier),
+        t.token("variable_4", TokenTypes.identifier),
         t.token(")", TokenTypes.rparen),
         t.token(";", TokenTypes.semicolon),
       ];
@@ -39,14 +41,14 @@ void main() {
       final interpreter = RhapsodyInterpreter(analysis);
       RhapsodyEvaluationContextBuilder builder =
           RhapsodyEvaluationContextBuilder(prefixes: ['env', 'config']);
-      builder.setRefValue('env:variable1', 'func1');
-      builder.setRefValue('env:variable4', 'func2');
+      builder.setRefValue('env:variable1:blue', 'func1');
+      builder.setRefValue('env:variable_4', 'func2');
       RhapsodyEvaluationContext context = builder.build();
       interpreter.interpret(context);
       expect(context.ruleState.get('rule1'), equals(RhapsodicBool.truth()));
       expect(context.ruleState.get('rule2'), equals(RhapsodicBool.truth()));
       // ---
-      context.variables.set('env:variable4', 'other');
+      context.variables.set('env:variable_4', 'other');
       interpreter.interpret(context);
       expect(context.ruleState.get('rule1'), equals(RhapsodicBool.untruth()));
       expect(context.ruleState.get('rule2'), equals(RhapsodicBool.untruth()));
